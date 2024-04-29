c74478d970e4af7c9ef4a5caffbc592fa5318954
munjji <ehowl1117@naver.com>
2024-04-30 00:03:16 +0900

    [feat] 폰트 추가 및 헤더 구현

--- moodoodleFront/moodoodle/src/App.js
+++ moodoodleFront/moodoodle/src/App.js
@@ -1,10 +1,29 @@
+import { BrowserRouter } from "react-router-dom";
+import { Mobile, Pc } from "./responsive";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>하이! 무두들러!</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <section className="w-[390px] h-[844px] flex flex-col m-auto">
+        <Pc className="flex flex-col m-auto">
+          <div className="k">
+            <Header />
+            <div className="">
+              <p>이것은 컴퓨터다.</p>
+            </div>
+          </div>
+        </Pc>
+        <Mobile className="flex flex-col m-auto">
+          <div className="">
+            <Header />
+            <div className="">
+              <p>이것은 모바일이다.</p>
+            </div>
+          </div>
+        </Mobile>
+      </section>
+    </BrowserRouter>
   );
 }
 
---
+++ moodoodleFront/moodoodle/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div className="flex items-center h-[64px] bg-white">
+      <div className="flex w-[230px] h-[22px] justify-between ml-[28px]">
+        <img src="/assets/bell.svg" alt="alarm" />
+        <Link to="/">
+          <img src="/assets/moodoodleLogo.svg" alt="logo" />
+        </Link>
+      </div>
+    </div>
+  );
+}
