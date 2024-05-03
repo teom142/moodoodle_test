99d498c45d935179e78f19978904105584af0bf6
munjji <ehowl1117@naver.com>
2024-05-03 17:19:53 +0900

    [style] 일기 작성 팝업 현재 날짜로 수정

--- moodoodleFront/moodoodlefront/src/components/DiaryWritePopup.jsx
+++ moodoodleFront/moodoodlefront/src/components/DiaryWritePopup.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+
 import CustomButton from './CustomButton';
 
 export default function DiaryWritePopup() {
+  const month = dayjs().format('MMM');
+  const year = dayjs().format('YYYY');
+  const day = dayjs().format('DD');
+
   return (
     <div className='flex justify-center items-center w-[342px] h-[159px] rounded-[20px] bg-white shadow-componentShadow'>
       <div className='flex flex-col justify-between items-center w-[175px] h-[99px]'>
@@ -10,7 +16,9 @@ export default function DiaryWritePopup() {
           <p className='font-bold text-base text-darkNavy'>
             오늘의 일기를 적어보아요!
           </p>
-          <p className='font-medium text-sm text-darkGray'>Apr. 16th, 2024</p>
+          <p className='font-medium text-sm text-darkGray'>
+            {month}. {day}, {year}
+          </p>
         </div>
         <Link to='/diary'>
           <CustomButton text='일기 쓰기' color='lemon' />
