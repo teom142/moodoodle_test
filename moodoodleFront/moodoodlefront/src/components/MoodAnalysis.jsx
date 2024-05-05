3228e4594664c4ed4a0e410e473a276dcdfbc3ae
munjji <ehowl1117@naver.com>
2024-05-05 20:23:51 +0900

    [feat] 감정 분석 api 구현

--- moodoodleFront/moodoodlefront/src/components/MoodAnalysis.jsx
+++ moodoodleFront/moodoodlefront/src/components/MoodAnalysis.jsx
@@ -2,14 +2,15 @@ import React, { useEffect } from 'react';
 import MoodHashTag from './MoodHashTag';
 import useDiaryAnalysis from '../hooks/useDiaryAnalysis';
 import useProfile from '../hooks/useProfile';
+import colorsByCode from '../constants/colorsByCode';
 
 export default function MoodAnalysis({ isModal, handleDateToggle, diary_id }) {
   const { analysisResult, getDiaryAnalysis } = useDiaryAnalysis();
   const { profile } = useProfile();
 
   useEffect(() => {
-    getDiaryAnalysis(diary_id);
-  }, [localStorage.getItem('selectedDay')]);
+    getDiaryAnalysis();
+  }, []);
 
   return (
     <div className='relative flex flex-col justify-center items-center w-[342px] h-[472px] rounded-[20px] bg-white shadow-componentShadow'>
@@ -17,16 +18,39 @@ export default function MoodAnalysis({ isModal, handleDateToggle, diary_id }) {
         <div className='flex flex-col h-[200px] justify-between items-center'>
           <p className='font-bold text-normal text-darkNavy'>감정 태그</p>
           <div className='flex flex-col h-[102px] justify-between items-center'>
-            <div className='w-[50px] h-[50px] rounded-full bg-darkGray' />
-            <div className='text-darkGray text-center text-[13px] whitespace-pre-line'>
-              [{profile.nickname}]님의 오늘 느낀 감정은 <br />
-              [행복]이 65% [슬픔]이 15% [힘듦]이 10%예요!
+            <div
+              className={`w-[50px] h-[50px] rounded-full ${colorsByCode.COLOR_LIST[analysisResult[0].mood_color].bg}`}
+            />
+            <div className='flex flex-col justify-center items-center'>
+              <div className='flex flex-row text-darkGray text-center text-[13px] whitespace-pre-line'>
+                [{profile.nickname}]님의 오늘 느낀 감정은
+              </div>
+              <div className='flex flex-row whitespace-pre-wrap text-darkGray text-center text-[13px]'>
+                {analysisResult.map((i, v) => (
+                  <div
+                    className='flex flex-row text-darkGray text-center text-[13px] whitespace-pre-wrap'
+                    key={v}
+                  >
+                    <p
+                      className={`${colorsByCode.COLOR_LIST[i.mood_color].text}`}
+                    >
+                      [{colorsByCode.COLOR_LIST[i.mood_color].mood_name}]
+                    </p>
+                    이 <b>{i.ratio}%</b>&nbsp;
+                  </div>
+                ))}
+                예요!
+              </div>
             </div>
           </div>
-          <div className='flex flex-row justify-center items-center gap-[25px]'>
-            <MoodHashTag />
-            <MoodHashTag />
-            <MoodHashTag />
+          <div className='flex flex-row flex-wrap justify-center items-center gap-[25px]'>
+            {analysisResult.map((i, v) => (
+              <MoodHashTag
+                key={v}
+                mood_title={i.mood_list[0].mood_title}
+                mood_color={i.mood_color}
+              />
+            ))}
           </div>
         </div>
         <div className='text-darkGray text-center text-[13px] whitespace-pre-line'>
