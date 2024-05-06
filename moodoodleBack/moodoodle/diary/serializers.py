3650b4bae087f123d99b6e9b397373fc0574c95a
daewoon <gkseodns1106@gmail.com>
2024-05-06 15:45:59 +0900

    [refactor] main_mood_color 수정

--- moodoodleBack/moodoodle/diary/serializers.py
+++ moodoodleBack/moodoodle/diary/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 
 from .models import Diary, diary_mood
 from collections import defaultdict
-from .mood_colors import mood_colors
 
 class DiaryCreateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -46,6 +45,15 @@ class MonthlyCalendarSerializer(serializers.ModelSerializer):
         fields = ('diary_id', 'date', 'content', 'main_mood_color')
 
     def get_main_mood_color(self, obj):
+        mood_colors = {
+            'B3F4B2': ['평온', '온화', '안정'],
+            'FEF4A0': ['성취감', '뿌듯함', '희망'],
+            'FBCFE0': ['행복', '즐거움', '기쁨'],
+            'FECFAD': ['열정', '자신감', '용기', '성공'],
+            'B5D3FF': ['슬픔', '우울', '피곤함', '지루함'],
+            'DBD3FB': ['불안', '공포', '놀람', '두려움'],
+            'FF9191': ['화남', '짜증', '분노', '예민']
+        }
         main_mood_ratio = defaultdict(int)
         mood_list = diary_mood.objects.filter(diary_id=obj)
         for mood in mood_list:
