be2bb4c664f9657b49cdb38103af3d90b957d218
munjji <ehowl1117@naver.com>
2024-05-07 20:36:15 +0900

    [feat] 일기 작성 페이지 글자수 제한 추가, 글자수 표현

--- moodoodleFront/moodoodlefront/src/components/DiaryWriting.jsx
+++ moodoodleFront/moodoodlefront/src/components/DiaryWriting.jsx
@@ -8,13 +8,19 @@ export default function DiaryWriting() {
   const navigate = useNavigate();
   const [content, setContent] = useState('');
   const selectedDay = localStorage.getItem('selectedDay');
+  let [inputCount, setInputCount] = useState(0);
 
   const handleSetValue = (e) => {
     setContent(e.target.value);
+    onInputHandler(e);
   };
 
   const handleSubmit = () => {
     navigate(`/analysis/${selectedDay}`);
+  };
+
+  const onInputHandler = (e) => {
+    setInputCount(e.target.value.length);
   };
   /*const handleSubmit = async () => {
     const postData = {
@@ -48,8 +54,15 @@ export default function DiaryWriting() {
           className='w-[307px] h-[256px] p-[17px] text-[14px] rounded-[20px] border-[0.8px] border-outlineGray outline-none resize-none'
           placeholder='오늘의 일기 내용을 입력해주세요.'
           value={content}
+          maxLength='300'
           onChange={(e) => handleSetValue(e)}
         />
+        <div className='flex flex-row w-[307px] justify-end'>
+          <p className='text-[11px] text-darkNavy'>
+            <span>{inputCount}</span>
+            <span>/300 자</span>
+          </p>
+        </div>
         <CustomButton
           text='등록 및 분석하기'
           color='pink'
