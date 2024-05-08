7d2e880573a6e09fe3f0ccd12d5a51e10f6ffa54
daewoon <gkseodns1106@gmail.com>
2024-05-08 22:12:22 +0900

    [feat] friend 예외 처리 추가

--- moodoodleBack/moodoodle/friend/views.py
+++ moodoodleBack/moodoodle/friend/views.py
@@ -9,31 +9,39 @@ from .serializers import FriendSerializer, FriendListSerializer
 
 class FriendListView(ListAPIView):
     serializer_class = FriendListSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        user_id = self.request.user.id
+        user_id = self.request.user.user_id
         user_friends = Friend.objects.filter(from_user_id=user_id) or Friend.objects.filter(to_user_id=user_id)
         friends_list = []
 
         for friends in user_friends:
             if Friend.objects.filter(to_user_id=friends.from_user_id, from_user_id=friends.to_user_id).exists():
                 friends_list.append(friends.to_user_id)
-        return users.object.filter(pk__in=friends_list)
+        return users.objects.filter(pk__in=friends_list)
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
-        serializer = self.serializer_class(queryset, many=True)
-        response = {
-            'success' : True,
-            'status code': status.HTTP_200_OK,
-            'message': '요청에 성공하였습니다.',
-            'result': serializer.data
-        }
+        if queryset.exists():
+            serializer = self.serializer_class(queryset, many=True)
+            response = {
+                'success': True,
+                'status_code': status.HTTP_200_OK,
+                'message': '요청에 성공하였습니다.',
+                'result': serializer.data
+            }
+        else:
+            response = {
+                'success': True,
+                'status_code': status.HTTP_200_OK,
+                'message': '친구 목록이 비었습니다.',
+                'result': []
+            }
         return Response(response, status=status.HTTP_200_OK)
 
 class FriendSearchView(RetrieveAPIView):
-    queryset = users.object.all()
+    queryset = users.objects.all()
     serializer_class = FriendListSerializer
     lookup_field = 'id'
 
@@ -60,38 +68,62 @@ class FriendSearchView(RetrieveAPIView):
 
 
 class FriendAddView(CreateAPIView):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     serializer_class = FriendSerializer
 
     def post(self, request, to_user_id):
-        from_user = request.user
+        from_user = self.request.user
+
+        if str(from_user.pk) == str(to_user_id):
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_400_BAD_REQUEST,
+                'message': '자신에게 친구 요청을 할 수 없습니다.'
+            }, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            to_user = users.object.get(pk=to_user_id)
+            to_user = users.objects.get(pk=to_user_id)
         except users.DoesNotExist:
-            response = {
-                'success' : False,
-                'status code': status.HTTP_404_NOT_FOUND,
-                'message': '유저를 찾을 수 없습니다.',
-            }
-            return Response(response, status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = self.serializer_class(data={'from_user': from_user.pk, 'to_user': to_user.pk})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        response = {
-            'success': True,
-            'status_code': status.HTTP_201_CREATED,
-            'message': '요청에 성공하였습니다.'
-        }
-        return Response(response, status=status.HTTP_201_CREATED)
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_404_NOT_FOUND,
+                'message': '유저를 찾을 수 없습니다.'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        if Friend.objects.filter(from_user=from_user, to_user=to_user).exists() or Friend.objects.filter(from_user=to_user, to_user=from_user).exists():
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_400_BAD_REQUEST,
+                'message': '이미 친구 관계가 설정되어 있습니다.'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        friend1 = {'from_user': from_user.pk, 'to_user': to_user.pk}
+        friend2 = {'from_user': to_user.pk, 'to_user': from_user.pk}
+
+        serializer1 = self.serializer_class(data=friend1)
+        serializer2 = self.serializer_class(data=friend2)
+
+        if serializer1.is_valid(raise_exception=True) and serializer2.is_valid(raise_exception=True):
+            serializer1.save()
+            serializer2.save()
+            return Response({
+                'success': True,
+                'status_code': status.HTTP_201_CREATED,
+                'message': '요청에 성공하였습니다.'
+            }, status=status.HTTP_201_CREATED)
+        else:
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_400_BAD_REQUEST,
+                'message': '요청에 실패하였습니다.'
+            }, status=status.HTTP_400_BAD_REQUEST)
 
 class FriendDeleteView(DestroyAPIView):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     serializer_class = FriendSerializer
 
     def delete(self, request, to_user_id):
-        from_user = request.user
+        from_user = self.request.user
 
         try:
             friends1 = Friend.objects.get(from_user=from_user, to_user_id=to_user_id)
