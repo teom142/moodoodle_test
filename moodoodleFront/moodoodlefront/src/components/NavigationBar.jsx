453b4880be23ac4e68f04c26086dd04b164fac27
munjji <ehowl1117@naver.com>
2024-05-08 22:44:24 +0900

    [style] 하단 탭 바 하단에 고정되게 수정

--- moodoodleFront/moodoodlefront/src/components/NavigationBar.jsx
+++ moodoodleFront/moodoodlefront/src/components/NavigationBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function NavigationBar() {
   return (
-    <div className='flex flex-row w-full h-[55px] justify-center items-center bg-white shadow-componentShadow'>
+    <div className='absolute fixed bottom-0 flex flex-row w-full h-[55px] justify-center items-center bg-white shadow-componentShadow'>
       <div className='flex flex-row w-[254px] h-[38px] justify-between items-end'>
         <Link to='/friend' className='flex flex-col items-center'>
           <img src='/assets/friend.svg' alt='friend' />
